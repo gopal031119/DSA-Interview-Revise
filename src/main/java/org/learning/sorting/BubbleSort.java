d6b1de38120c424da33f0ac90f3c06b8671c28c5
Gopal Sakhwala <Gopal.Sakhwala@einfochips.com>
2024-05-01 13:23:42 +0530

    sorting part 1 learning

---
+++ src/main/java/org/learning/sorting/BubbleSort.java
@@ -0,0 +1,30 @@
+package org.learning.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String... args) {
+        bubbleSort(new int[]{6, 5, 4, 3, 2, 1});
+    }
+
+    /**
+     * Time Complexity - O(n2)
+     *
+     * @param arr
+     */
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int swap = 0;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swap++;
+                }
+                if (swap == 0) break;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
